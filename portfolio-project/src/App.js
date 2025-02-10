3e43999cba0b59ea6770c0957a29d3726747f62e
Carlos González <ll3224645@gmail.com>
2025-02-10 16:17:05 -0600

    Agregando modulo de cambio de Tema

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Modules/SwitchTheme/SwitchTheme';
+import SwitchTheme from './Modules/SwitchTheme/SwitchTheme';
 
 function App() {
   return (
     <div className="App">
-      
+      <SwitchTheme />
     </div>
   );
 }
